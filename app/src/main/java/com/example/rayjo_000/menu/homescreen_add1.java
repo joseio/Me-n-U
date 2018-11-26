4d5030dd4f733d0a58465d6fb116b8baec4b3adc
Trent Sanford <trentsanf8196@gmail.com>
2018-11-26 16:59:56 -0600

    Initial Commit
    layout for step 1 is done, and step 2 is created

---
+++ app/src/main/java/com/example/rayjo_000/menu/homescreen_add1.java
@@ -0,0 +1,13 @@
+package com.example.rayjo_000.menu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class homescreen_add1 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homescreen_add1);
+    }
+}
---
+++ app/src/main/java/com/example/rayjo_000/menu/homescreen_add2.java
@@ -0,0 +1,13 @@
+package com.example.rayjo_000.menu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class homescreen_add2 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homescreen_add2);
+    }
+}
