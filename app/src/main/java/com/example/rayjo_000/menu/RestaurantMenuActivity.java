70e2619bcf086433330f86269c800ff987e8d38c
Edward Kim <kim.edwardj@gmail.com>
2018-11-28 21:27:35 -0600

    Added restaurant menu page

---
+++ app/src/main/java/com/example/rayjo_000/menu/RestaurantMenuActivity.java
@@ -0,0 +1,13 @@
+package com.example.rayjo_000.menu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RestaurantMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_menu);
+    }
+}
