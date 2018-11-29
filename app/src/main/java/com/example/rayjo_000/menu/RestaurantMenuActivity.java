49b38e9991de8b5048f10e33bd1a494a57eedec9
Edward Kim <kim.edwardj@gmail.com>
2018-11-28 23:21:06 -0600

    doneeee

--- app/src/main/java/com/example/rayjo_000/menu/RestaurantMenuActivity.java
+++ app/src/main/java/com/example/rayjo_000/menu/RestaurantMenuActivity.java
@@ -1,7 +1,9 @@
 package com.example.rayjo_000.menu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RestaurantMenuActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class RestaurantMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_menu);
+
+        View view2 = findViewById(R.id.view2);
+        view2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), orange_chicken_item.class));
+            }
+        });
+
+        View view3 = findViewById(R.id.view3);
+        view3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), AddMenuItem_1.class));
+            }
+        });
     }
 }
