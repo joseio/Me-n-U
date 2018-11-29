b1456bbdcb50093f34679e0f2ea230c48c617d92
jo28 <jo28@illinois.edu>
2018-11-28 23:00:02 -0600

    - Can swipe b/t resulting dishes after having searched for "chinese"

---
+++ app/src/main/java/com/example/rayjo_000/menu/ChinesePage1.java
@@ -0,0 +1,26 @@
+package com.example.rayjo_000.menu;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class ChinesePage1 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.chinese_page_1, container, false);
+        ImageView img = rootView.findViewById(R.id.imageView9);
+        int width = 332;
+        int height = 172;
+        Bitmap bitmap = ((BitmapDrawable) getResources().getDrawable(R.drawable.dumplings)).getBitmap();
+        bitmap = Bitmap.createScaledBitmap(bitmap, width, height, false);
+        img.setImageBitmap(bitmap);
+        return rootView;
+    }
+}
