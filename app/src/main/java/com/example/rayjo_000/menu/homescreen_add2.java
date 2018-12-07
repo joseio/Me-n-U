6a3636d5b9a93764db6548e5d51a724aac6af1ff
Trent Sanford <trentsanf8196@gmail.com>
2018-12-06 23:30:35 -0600

    Added Notification reminder and the task flow is correct on my Galaxy S7 (Android 8.0)

--- app/src/main/java/com/example/rayjo_000/menu/homescreen_add2.java
+++ app/src/main/java/com/example/rayjo_000/menu/homescreen_add2.java
@@ -1,8 +1,14 @@
 package com.example.rayjo_000.menu;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.NotificationChannel;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.support.v4.app.NotificationCompat;
 import android.widget.Button;
 import android.widget.Toast;
 import android.graphics.BitmapFactory;
@@ -30,12 +36,12 @@ public class homescreen_add2 extends Activity {
 
         Button finish_btn = (Button)findViewById(R.id.hmscreen_addf);
 
-        finish_btn.setOnClickListener(new View.OnClickListener() {
+      /*  finish_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(homescreen_add2.this, AddMenuItem_RateIt.class));
             }
-        });
+        });*/
     }
 
 
@@ -63,6 +69,33 @@ public class homescreen_add2 extends Activity {
                 }
                 break;
         }
+    }
+
+    public void sendNotification(View view) {
+
+        int notifyid = 1;
+        String CHANNEL_ID = "my_channel_01";
+        CharSequence name = getString(R.string.channel_name);// The user-visible name of the channel.
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID);
+
+        Intent rateintent = new Intent(this, AddMenuItem_RateIt.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,rateintent,0);
+
+        mBuilder.setContentIntent(pendingIntent);
+        mBuilder.setSmallIcon(R.drawable.common_full_open_on_phone);
+        mBuilder.setContentTitle("Me 'n U");
+        mBuilder.setContentText("Don't Forget to Rate the Food!!!");
+        mBuilder.setChannelId(CHANNEL_ID);
+
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.createNotificationChannel(mChannel);
+
+        mNotificationManager.notify(notifyid, mBuilder.build());
+
+        startActivity(new Intent(homescreen_add2.this, MainActivity.class));
     }
 
 }
